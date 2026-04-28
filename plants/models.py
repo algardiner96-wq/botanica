1434b804bd7d049627bd865814cd2b713a9c0da4
Al <algardiner96@gmail.com>
2026-04-28 17:05:10 +0100

    create garden and platn app along with plant model

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100, blank=True, null=True)
+    plant_type = models.CharField(max_length=50)
+    watering = models.CharField(max_length=50)
+    sunlight = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    icon = models.ImageField(upload_to='plant_icons/', blank=True, null=True)
+    image = models.ImageField(upload_to='plant_images/', blank=True, null=True)
+    symbolism = models.ManyToManyField('Symbolism', blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    
