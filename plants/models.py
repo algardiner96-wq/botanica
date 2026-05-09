5aa533057e0fd9a67af03ef647731733c6689324
Al <algardiner96@gmail.com>
2026-05-09 15:49:12 +0100

    fix plant model

--- plants/models.py
+++ plants/models.py
@@ -7,13 +7,41 @@ class Plant(models.Model):
         ('Perennial', 'Perennial'),
         ('Biennial', 'Biennial'),
     ]
+
+    WATERING_CHOICES = [
+        ("dry", "Dry / Drought-tolerant"),
+        ("light", "Light / Low water needs"),
+        ("moderate", "Moderate / regular watering"),
+        ("moist", "Moist / high water needs"),
+    ]
+
+    SUNLIGHT_CHOICES = [
+        ("full_sun", "Full Sun"),
+        ("partial_sun", "Partial Sun"),
+        ("shade", "Shade"),
+    ]
+
+    PLANT_TYPE_CHOICES = [
+        ("herb", "Herb"),
+        ("vegetable", "Vegetable"),
+        ("fruit", "Fruit"),
+        ("flower", "Flower"),
+        ("shrub", "Shrub"),
+        ("tree", "Tree"),
+        ("succulent", "Succulent"),
+        ("fern", "Fern"),
+        ("grass", "Grass"),
+        ("vine", "Vine")
+    ]
     
+
+
     name = models.CharField(max_length=100)
     scientific_name = models.CharField(max_length=100, blank=True, null=True)
-    plant_type = models.CharField(max_length=50)
+    plant_type = models.CharField(max_length=50, choices=PLANT_TYPE_CHOICES, blank=True)
     lifespan = models.CharField(max_length=20, choices=LIFESPAN_CHOICES, blank=True)
-    watering = models.CharField(max_length=50)
-    sunlight = models.CharField(max_length=50)
+    watering = models.CharField(max_length=50, choices=WATERING_CHOICES, blank=True)
+    sunlight = models.CharField(max_length=50, choices=SUNLIGHT_CHOICES, blank=True)
     description = models.TextField(blank=True)
     icon = models.ImageField(upload_to='plant_icons/', blank=True, null=True)
     image = models.ImageField(upload_to='plant_images/', blank=True, null=True)
